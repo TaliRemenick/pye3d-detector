94021c30690afcb76aaf9fa9ff618f1b41946649
tali <taliremenick@mail.tau.ac.il>
2023-09-26 12:05:46 +0300

    changed constants

--- pye3d/constants.py
+++ pye3d/constants.py
@@ -1,4 +1,4 @@
 import typing as T
 
-_EYE_RADIUS_DEFAULT: float = 10.392304845413264
-DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 35.0)
+_EYE_RADIUS_DEFAULT: float = 5.392304845413264
+DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 15.0)
