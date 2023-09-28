d0c156a04ce59cd55ca2b0ef9bbf1d40c4de798b
tali <taliremenick@mail.tau.ac.il>
2023-09-28 11:46:05 +0300

    added prints for debugging

--- pye3d/observation.py
+++ pye3d/observation.py
@@ -211,10 +211,13 @@ class BinBufferedObservationStorage(ObservationStorage):
 
     def _get_bin(self, observation: Observation) -> int:
         x, y = (
-            floor((ellipse_center + resolution / 2) / self.pixels_per_bin)
+            print(f"ellipse center - {ellipse_center}, resolution - {resolution}")
+            # floor((ellipse_center + resolution / 2) / self.pixels_per_bin)
             for ellipse_center, resolution in zip(
                 observation.ellipse.center, self.camera.resolution
             )
+
+
         )
         print(f"x: {x}, y: {y}")
         # convert to 1D bin index
