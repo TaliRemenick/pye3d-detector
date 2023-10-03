9bea7bf49ebbfc7ad2e508672d6c83f06d373f3f
tali <taliremenick@mail.tau.ac.il>
2023-10-03 15:07:21 +0300

    changed default in eye radius

--- pye3d/constants.py
+++ pye3d/constants.py
@@ -1,4 +1,4 @@
 import typing as T
 
 _EYE_RADIUS_DEFAULT: float = 1
-DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 20)
+DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 35)
