114ff7523fe6aac7610eac65032fc8a72741d08e
TaliRemenick <taliremenick@mail.tau.ac.il>
2023-11-26 12:07:27 +0200

    comment from python script

--- pye3d/constants.py
+++ pye3d/constants.py
@@ -1,3 +1,3 @@
 import typing as T
-_EYE_RADIUS_DEFAULT: float = 80
-DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 4000)
+_EYE_RADIUS_DEFAULT: float = 90
+DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 600)
