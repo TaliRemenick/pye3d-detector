88b1f9eb7e520f190fb68acac6b7357837d60395
TaliRemenick <taliremenick@mail.tau.ac.il>
2023-12-11 16:00:41 +0200

    changes from python, r = 5, s = 100

--- pye3d/constants.py
+++ pye3d/constants.py
@@ -1,3 +1,3 @@
 import typing as T
 _EYE_RADIUS_DEFAULT: float = 5
-DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 10)
+DEFAULT_SPHERE_CENTER: T.Tuple[float, float, float] = (0.0, 0.0, 100)
